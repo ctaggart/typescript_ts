58d4731800efde887fa5db530e81963d83089f0b
Cameron Taggart <cameron.taggart@gmail.com>
2018-03-14 17:18:32 -0500

    all as_* "casts"

--- examples/create_node.rs
+++ examples/create_node.rs
@@ -6,8 +6,6 @@ fn main() {
     let js = ts::Js::new(&guard);
     let ts = js.ts();
 
-//    let kind = &ts::SyntaxKind { id: ts::SyntaxKindEnum::MultiLineCommentTrivia }; // TODO
-//    let kind = &ts::SyntaxKind { id: 3 };
-    let node = ts.createNode(ts::SyntaxKindEnum::MultiLineCommentTrivia, None, None);
+    let node = ts.createNode(ts::SyntaxKind::MultiLineCommentTrivia, None, None);
     println!("kind: {:?}", node.kind());
 }
